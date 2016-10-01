18198018296727b11a2097eaf460089558bb8cdc
kickinbahk <kickinbahk@gmail.com>
2016-10-01 14:53:13 -0700

    add data and multiple items.

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -9,12 +9,28 @@ class ChecklistViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView,
                           numberOfRowsInSection section: Int) -> Int {
-    return 5
+    return 100
   }
   
   override func tableView(_ tableView: UITableView,
                           cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
     let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+
+    let label = cell.viewWithTag(1000) as! UILabel
+    
+    if indexPath.row % 5 == 0 {
+      label.text = "Walk the Dog"
+    } else if indexPath.row % 5 == 1 {
+      label.text = "Brush My Teeth"
+    } else if indexPath.row % 5 == 2 {
+      label.text = "Learn iOS Development"
+    } else if indexPath.row % 5 == 3 {
+      label.text = "Catch the Big Game"
+    } else if indexPath.row % 5 == 4 {
+      label.text = "Eat Ice Cream"
+    }
+    
     return cell
   }
 
