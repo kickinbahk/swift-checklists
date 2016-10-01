db836b780af33767e88c691bff052b0524c10574
kickinbahk <kickinbahk@gmail.com>
2016-10-01 14:18:33 -0700

    add function to display cells based off storyboard prototype cell

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -6,6 +6,17 @@ class ChecklistViewController: UITableViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
+  
+  override func tableView(_ tableView: UITableView,
+                          numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  override func tableView(_ tableView: UITableView,
+                          cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+    return cell
+  }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
