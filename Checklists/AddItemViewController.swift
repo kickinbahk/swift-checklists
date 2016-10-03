3e0c524831b3c79df31d2a9af060d84cade7fd9c
kickinbahk <kickinbahk@gmail.com>
2016-10-03 16:41:20 -0700

    add text field to add item

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -3,4 +3,25 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
   
+  override func tableView(_ tableView: UITableView,
+                          willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+    return nil
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    textField.becomeFirstResponder()
+  }
+  
+  @IBOutlet weak var textField: UITextField!
+  
+  @IBAction func cancel() {
+    dismiss(animated: true, completion: nil)
+  }
+  
+
+  @IBAction func done() {
+    print("Contents of the Text field: \(textField.text)")
+    dismiss(animated: true, completion: nil)
+  }
 }
