4dd253795dee5a34855d130163f9e0d64e08c609
kickinbahk <kickinbahk@gmail.com>
2016-10-03 17:29:00 -0700

    change addItem controller to be textField delegate, check for text field length to activate done button

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -1,7 +1,11 @@
 import Foundation
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+  
+  @IBOutlet weak var doneBarButton: UIBarButtonItem!
+  
+  @IBOutlet weak var textField: UITextField!
   
   override func tableView(_ tableView: UITableView,
                           willSelectRowAt indexPath: IndexPath) -> IndexPath? {
@@ -13,8 +17,6 @@ class AddItemViewController: UITableViewController {
     textField.becomeFirstResponder()
   }
   
-  @IBOutlet weak var textField: UITextField!
-  
   @IBAction func cancel() {
     dismiss(animated: true, completion: nil)
   }
@@ -23,5 +25,19 @@ class AddItemViewController: UITableViewController {
   @IBAction func done() {
     print("Contents of the Text field: \(textField.text)")
     dismiss(animated: true, completion: nil)
+  }
+
+  func textField(_ textField: UITextField,
+                 shouldChangeCharactersIn range: NSRange,
+                 replacementString string: String) -> Bool {
+    
+    let oldText = textField.text! as NSString
+    let newText = oldText.replacingCharacters(in: range, with: string) as NSString
+    
+    doneBarButton.isEnabled = (newText.length > 0)
+    
+    // TODO: Clear also grays out done button. 
+    
+    return true
   }
 }
