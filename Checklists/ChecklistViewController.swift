b767138b9c7e8dc8ee40bb3a6adeb740956a1bed
kickinbahk <kickinbahk@gmail.com>
2016-10-04 08:49:59 -0700

    get notification when nav buttons are clicked, passalong checklist item to edit to controller

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -86,6 +86,17 @@ class ChecklistViewController: UITableViewController,
       let controller = navigationController.topViewController
                                                 as!AddItemViewController
       controller.delegate = self
+    } else if segue.identifier == "EditItem" {
+      let navigationController = segue.destination
+                                                as! UINavigationController
+      let controller = navigationController.topViewController
+                                                as! AddItemViewController
+      controller.delegate = self
+      
+      if let indexPath = tableView.indexPath(
+        for: sender as! UITableViewCell) {
+        controller.itemToEdit = items[indexPath.row]
+      }
     }
   }
   
