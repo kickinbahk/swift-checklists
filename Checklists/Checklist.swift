8f67e0527c820c91622248a3f7b7942b5e53115c
kickinbahk <kickinbahk@gmail.com>
2016-10-06 07:11:03 -0700

    add new file for checklist obj

---
+++ Checklists/Checklist.swift
@@ -0,0 +1,13 @@
+//
+//  Checklist.swift
+//  Checklists
+//
+//  Created by Josiah Mory on 10/6/16.
+//  Copyright © 2016 kickinbahk Productions. All rights reserved.
+//
+
+import UIKit
+
+class Checklist: NSObject {
+
+}
