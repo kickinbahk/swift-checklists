8e9913b7a2c65c2015b5ae352846353a601da328
kickinbahk <kickinbahk@gmail.com>
2016-10-08 15:54:49 -0700

    implement method to hide date picker

--- Checklists/ItemDetailViewController.swift
+++ Checklists/ItemDetailViewController.swift
@@ -79,7 +79,11 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
     editItemTextField.resignFirstResponder()
     
     if indexPath.section == 1 && indexPath.row == 1 {
-      showDatePicker()
+      if !datePickerVisible {
+        showDatePicker()
+      } else {
+        hideDatePicker()
+      }
     }
   }
   
@@ -137,6 +141,10 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
                                                                   .length > 0)
     return true
   }
+  
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    hideDatePicker()
+  }
 
   func textFieldShouldClear(_ textField: UITextField) -> Bool {
       doneBarButton.isEnabled = false
@@ -157,6 +165,24 @@ class ItemDetailViewController: UITableViewController, UITextFieldDelegate {
     tableView.endUpdates()
     
     datePicker.setDate(dueDate, animated: false)
+  }
+  
+  func hideDatePicker() {
+    if datePickerVisible {
+      datePickerVisible = false
+      
+      let indexPathDateRow = IndexPath(row: 1, section: 1)
+      let indexPathDatePicker = IndexPath(row: 2, section: 1)
+      
+      if let cell = tableView.cellForRow(at: indexPathDateRow) {
+        cell.detailTextLabel!.textColor = UIColor(white: 0, alpha: 0.5)
+      }
+      
+      tableView.beginUpdates()
+      tableView.reloadRows(at: [indexPathDateRow], with: .none)
+      tableView.deleteRows(at: [indexPathDatePicker], with: .fade)
+      tableView.endUpdates()
+    }
   }
   
   func updateDueDateLabel() {
